585165ebac331461d8d373f33393cdf126817129
Miyano <dr000w@gmail.com>
2016-05-07 13:57:22 -0400

    Style to Weather page

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,7 +15,7 @@ var Weather = React.createClass({
     var that = this;
 
     this.setState({ isLoading: true });
-    
+
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
@@ -34,7 +34,7 @@ var Weather = React.createClass({
 
     function renderMessage () {
       if (isLoading) {
-        return <h3>Fetching weather...</h3>
+        return <h3 className="text-center">Fetching weather...</h3>
       } else if (temp && location) {
         return <WeatherMessage location={location} temp={temp}/>
       }
@@ -42,9 +42,9 @@ var Weather = React.createClass({
 
     return (
       <div>
-        <h3>Get Weather</h3>
+        <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
-      {renderMessage()}
+        {renderMessage()}
       </div>
     );
   }
