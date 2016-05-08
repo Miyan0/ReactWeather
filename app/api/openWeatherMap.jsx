eea7138e1699070b3068c5c79957aa788aef9b6d
Miyano <dr000w@gmail.com>
2016-05-08 00:28:41 -0400

    Added error message

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,7 +1,8 @@
 var axios = require('axios');
 
 // api key: 23212bbf2cb83fc09746e45e5e543576
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=23212bbf2cb83fc09746e45e5e543576&units=metric';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=23212bbf2cb83fc09746e45e5e543576&units=imperial';
+
 
 module.exports = {
   getTemp: function (location) {
@@ -10,12 +11,12 @@ module.exports = {
 
     return axios.get(requestUrl).then(function (res) {
       if (res.data.cod && res.data.message) {
-        throw new Error (res.data.message);
+        throw new Error(res.data.message);
       } else {
         return res.data.main.temp;
       }
     }, function (res) {
-      throw new Error (res.date.message);
+      throw new Error(res.data.message);
     });
   }
 }
